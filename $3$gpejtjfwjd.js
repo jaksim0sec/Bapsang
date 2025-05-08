c61b1a58a368ce3666ac5b351fb33379f9a5b1f6
박영초 park0sec <155617741+jaksim0sec@users.noreply.github.com>
2025-05-08 23:48:03 +0900

    Update + 0.085

--- $3$gpejtjfwjd.js
+++ $3$gpejtjfwjd.js
@@ -1,7 +1,7 @@
 document.head.appendChild(Object.assign(document.createElement('meta'), { charset: 'UTF-8' }));
 document.head.appendChild(Object.assign(document.createElement('meta'), { name: 'viewport', content: 'width=device-width' }));
 document.head.appendChild(Object.assign(document.createElement('link'), { rel: 'stylesheet', href: 'styles.css' }));
-document.head.appendChild(Object.assign(document.createElement('link'), { rel: 'shortcut icon', href: 'https://ifh.cc/g/tVs9Jo.png' }));
+document.head.appendChild(Object.assign(document.createElement('link'), { rel: 'shortcut icon', href: 'https://ifh.cc/g/ZjHYT7.png' }));
 document.head.appendChild(Object.assign(document.createElement('link'), { rel: 'preconnect', href: 'https://fonts.googleapis.com' }));
 document.head.appendChild(Object.assign(document.createElement('link'), { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' }));
 document.head.appendChild(Object.assign(document.createElement('link'), { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=42dot+Sans:wght@300..800&display=swap' }));
